3b017fa865a80e4ea58deb1a85fbec67f32ed86d
afaq01ahmad <afaq.ahmad@emerald-labs.com>
2023-11-09 18:30:12 +0500

    Nav Bar created. First page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import Aos from "aos";
 import "./App.css";
-
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Hero from "./Components/UI/Hero";
 function App() {
+  useEffect(() => {
+    Aos.init();
+  });
   return (
     <>
-      <div>hello World</div>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+      <Footer />
     </>
   );
 }
